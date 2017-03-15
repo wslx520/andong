ca3f66ef73003cfba75204d4ece6b044f107c6a7
李兴 <lixing@jingoal.com>
2017-03-15 15:42:05 +0800

    添加scrollpin。完善scrolltodo
    置入一个getscroll函数解决兼容

--- js/plugin/ScrollToDo.js
+++ js/plugin/ScrollToDo.js
@@ -14,13 +14,42 @@
     var types = {
         'scrollbottom': 1, 'scrolltop': 1, 'scrollleft': 1, 'scrollright': 1, 'scroll': 1
     };
+    // 根据 scroller 返回一个能正确获取 scrollTop 与 scrollLeft 的函数
+    var toGetScroll = function (scroller) {
+        if (scroller === window || scroller === document.body || scroller === document.documentElement) {
+            if (typeof window.pageYOffset === 'undefined') {
+                return function () {
+                    return {
+                        scrollLeft: Math.max(document.documentElement.scrollLeft, document.body.scrollLeft),
+                        scrollTop: Math.max(document.documentElement.scrollTop, document.body.scrollTop)
+                    }
+                }
+            }
+            return function () {
+                return {
+                    scrollLeft: window.pageXOffset,
+                    scrollTop: window.pageYOffset
+                }
+            }
+        }
+        return function () {
+            return {
+                scrollLeft: scroller.scrollLeft,
+                scrollTop: scroller.scrollTop
+            }
+        }
+    };
     function ScrollToDo(ops) {
         var root = this;
         root.scroller = typeof ops.scroller === 'string' ? document.querySelector(ops.scroller) : ops.scroller;
+        var bindTo = root.scroller;
         if (root.scroller.length) {
             root.scroller = root.scroller[0];
         }
         var scroller = root.scroller;
+        var getScroll = toGetScroll(scroller);
+        // scroll 事件节流器 timer
+        var _timer = null;
         // var callbackPrefix = '__';
         root.callbacks = {};
         // 支持以下事件监听
@@ -34,21 +63,22 @@
             var X = 0;
             // 1为向下，-1为向上
             var Y = 0;
-            if (scroller.scrollTop - lastScrollTop > 0) {
+            var scrolls = root.scrolls = getScroll();
+            if (scrolls.scrollTop - lastScrollTop > 0) {
                 Y = 1;
             }
-            else if (scroller.scrollTop - lastScrollTop < 0) {
+            else if (scrolls.scrollTop - lastScrollTop < 0) {
                 Y = -1;
             }
-            if (scroller.scrollLeft - lastScrollLeft > 0) {
+            if (scrolls.scrollLeft - lastScrollLeft > 0) {
                 X = 1;
             }
-            else if (scroller.scrollLeft - lastScrollLeft < 0) {
+            else if (scrolls.scrollLeft - lastScrollLeft < 0) {
                 X = -1;
             }
             setTimeout(function () {
-                scroller.lastScrollTop = scroller.scrollTop;
-                scroller.lastScrollLeft = scroller.scrollLeft;
+                scroller.lastScrollTop = scrolls.scrollTop;
+                scroller.lastScrollLeft = scrolls.scrollLeft;
             }, 0);
 
 
@@ -57,25 +87,33 @@
                 Y: Y
             };
         };
-        scroller.addEventListener('scroll', function () {
-            var directions = root.directions = root.getDirection();
-            root.directionX = directions.X;
-            root.directionY = directions.Y;
-            root.scrollTop = scroller.scrollTop;
-            root.scrollLeft = scroller.scrollLeft;
-            if (root.directionY === -1) {
-                root.fire('scrolltop');
-            }
-            else if (root.directionY === 1) {
-                root.fire('scrollbottom');
-            }
-            if (root.directionX === -1) {
-                root.fire('scrollleft');
-            }
-            else if (root.directionX === 1) {
-                root.fire('scrollright');
-            }
-            root.fire('scroll');
+        // document.documentElement 并没有 onscroll事件；document.body 的 onscroll 兼容性也不好
+        // 所以，若 scroller 是 document.documentElement 或 body 时，则将 onscroll 监听加在 window 上
+        if (scroller === document.body || scroller === document.documentElement) {
+            bindTo = window;
+        }
+        bindTo.addEventListener('scroll', function () {
+            window.clearTimeout(_timer);
+            _timer = setTimeout(function () {
+                var directions = root.directions = root.getDirection();
+                root.directionX = directions.X;
+                root.directionY = directions.Y;
+                root.scrollTop = root.scrolls.scrollTop;
+                root.scrollLeft = root.scrolls.scrollLeft;
+                if (root.directionY === -1) {
+                    root.fire('scrolltop');
+                }
+                else if (root.directionY === 1) {
+                    root.fire('scrollbottom');
+                }
+                if (root.directionX === -1) {
+                    root.fire('scrollleft');
+                }
+                else if (root.directionX === 1) {
+                    root.fire('scrollright');
+                }
+                root.fire('scroll');    
+            }, 20);            
         });
     }
     ScrollToDo.prototype = {
--- js/products.js
+++ js/products.js
@@ -1,5 +1,14 @@
 define(function (require, exports, module) {
 	var $ = require('lib/jquery');
-	var ScrollToDo = require('plugin/ScrollToDo');
+	var ScrollPin = require('plugin/ScrollPin');
+    var scrollPin = new ScrollPin({
+            pinner: $('#will-fixed'),
+            // 不同尺寸的屏幕下，此高度可能不同
+            boundary: $('#will-fixed').offset().top,
+            scroller: window,
+            ontoggle: function () {
+                
+            }
+        });
 });
 
