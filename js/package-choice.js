bd6ef913939c974eddba928d283375293c4d5830
wslx520 <lix9nw@gmail.com>
2017-03-20 12:15:52 +0800

    choice  modal

--- js/package-choice.js
+++ js/package-choice.js
@@ -2,6 +2,19 @@
 define(function (require, exports, module) {
 	var $ = window.jQuery = require('lib/jquery');
 	var bs = require('../bootstrap/js/bootstrap.min');
-
+	
+	var selects = $('.select-box');
+	selects.on('click', function (e) {
+		selects.not(this).find('.list').hide();
+		var select = $(this);
+		select.find('.list').toggle();
+		var target = e.target;
+		if (target.tagName === "LI") {
+			select.find('.list li').removeClass('on');
+			$(target).addClass('on');
+			select.find('em').text($(target).text());
+			select.find('input').val($(target).attr('data-value'));
+		}
+	});
 });
 
