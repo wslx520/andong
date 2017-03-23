44718d74f260af43f9c65fea5ce7995f67bef9d5
李兴 <lixing@jingoal.com>
2017-03-23 15:47:34 +0800

    bug补

--- js/package.js
+++ js/package.js
@@ -1,6 +1,19 @@
 define(function (require, exports, module) {
 	var $ = require('lib/jquery');
 	var head = require('head');
+    var bs = require('../bootstrap/js/bootstrap.min');
+
+
+    var ScrollPin = require('plugin/ScrollPin');
+    var scrollPin = new ScrollPin({
+            pinner: $('#will-fixed'),
+            // 不同尺寸的屏幕下，此高度可能不同
+            boundary: $('#will-fixed').offset().top,
+            scroller: window,
+            ontoggle: function () {
+                
+            }
+        });
 	var XScroll = require('plugin/XScroll2');
 	var xscroll = XScroll('banner-pics', 
 		{auto:4000,how:1, direct:0,pager:'banner-pager', event: 'click'}
@@ -16,9 +29,9 @@ define(function (require, exports, module) {
 	$('#table-stuff').on('click', 'td', function (e) {
 		var t = this.getAttribute('data-target');
 		if (t) {
-			$('.stuff').removeClass('on');
+			$('.stuff.on').removeClass('on');
 			$(t).addClass('on');
-			$('#table-stuff td').removeClass('on');
+			$('#table-stuff td.on').removeClass('on');
 			$(this).addClass('on');
 		}
 	})
